394432328cf0a383d63de19599c0b60af49e12a9
D. V. Wiebe <dvw@phas.ubc.ca>
2019-09-11 18:30:12 -0700

    blacken

--- chimedb/core/connectdb.py
+++ chimedb/core/connectdb.py
@@ -97,12 +97,7 @@ These constants tell the module how to connect to the CHIME database.
 """
 
 # === Start Python 2/3 compatibility
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 # Explicity avoid importing int: sshtunnel requires port numbers
 # be native int objects.
@@ -220,9 +215,7 @@ class RetryOperationalError(object):
 
     def execute_sql(self, sql, params=None, commit=True):
         try:
-            cursor = super(RetryOperationalError, self).execute_sql(
-                sql, params, commit
-            )
+            cursor = super(RetryOperationalError, self).execute_sql(sql, params, commit)
         except pw.OperationalError:
             if not self.is_closed():
                 self.close()
@@ -322,9 +315,7 @@ class BaseConnector(object):
             )
         else:
             ctx = " in {0}".format(context) if context else ""
-            raise ValueError(
-                "Invalid database type ({0})".format(d["db_type"]) + ctx
-            )
+            raise ValueError("Invalid database type ({0})".format(d["db_type"]) + ctx)
 
 
 class MySQLConnector(BaseConnector):
@@ -401,11 +392,7 @@ class MySQLConnector(BaseConnector):
         host, port = self._host_port()
         try:
             self._database = MySQLDatabaseReconnect(
-                self._db,
-                host=host,
-                port=port,
-                user=self._user,
-                passwd=self._passwd,
+                self._db, host=host, port=port, user=self._user, passwd=self._passwd
             )
         except None:
             # TODO More descriptive here.
@@ -416,9 +403,7 @@ class MySQLConnector(BaseConnector):
     def description(self):
         out = "MySQL database at %s port %d" % (self._host, self._port)
         if self._tunnel_host:
-            out += " tunnelled through {0} to localhost".format(
-                self._tunnel_host
-            )
+            out += " tunnelled through {0} to localhost".format(self._tunnel_host)
             if self._tunnel_port is not None:
                 out += " port {0}".format(self._tunnel_port)
         return out
@@ -481,9 +466,7 @@ class MySQLConnector(BaseConnector):
         self._tunnel.skip_tunnel_checkup = False
         self._tunnel.check_tunnels()  # This waits for the tunnel to come up
         if not self._tunnel.tunnel_is_up[(_LOCALHOST, self._tunnel_port)]:
-            raise ConnectionError(
-                "An error occurred while setting up the tunnel."
-            )
+            raise ConnectionError("An error occurred while setting up the tunnel.")
 
     def close(self):
         """Close an open connection."""
@@ -590,9 +573,7 @@ def create_tunnel(
         )
 
     if not tunnel_active(tunnel_port):
-        msg = (
-            "SSH tunnel to DB inactive: creating one with command %s." % command
-        )
+        msg = "SSH tunnel to DB inactive: creating one with command %s." % command
         _logger.info(msg)
         # print command    # Eventually get rid of this.
         pipe = os.popen(command, "r")
@@ -660,9 +641,7 @@ def _try_rc_files():
                 # Create the connectors
                 for section in sections:
                     if section in rc:
-                        conn[section] = BaseConnector.from_dict(
-                            rc[section], rc_file
-                        )
+                        conn[section] = BaseConnector.from_dict(rc[section], rc_file)
 
                 # It's all or nothing
                 if "connector" in conn:
@@ -675,11 +654,7 @@ File "{0}" defines 'connector' but not 'connector_rw'.""".format(
                             )
                         )
                     else:
-                        return (
-                            [conn["connector"]],
-                            [conn["connector_rw"]],
-                            rc_file,
-                        )
+                        return ([conn["connector"]], [conn["connector_rw"]], rc_file)
                 elif "connector_rw" in conn:
                     raise ConnectionError(
                         """
@@ -762,6 +737,7 @@ locations or install `chimedb.config`."""
 
     if current_connector is None or current_connector_RW is None:
         raise ConnectionError()
+
 
 def close():
     """Close all open database connections."""
