6b7314ebace2c8b0ed4ac0efe2e4cee6b221c334
Rick Nitsche <rick@phas.ubc.ca>
2020-07-20 16:12:56 -0700

    ci(travis): use pytest

--- setup.py
+++ setup.py
@@ -24,4 +24,5 @@ setup(
     description="Low-level CHIME database access",
     license="GPL v3.0",
     url="https://github.com/chime-experiment/chimedb",
+    extras_require={"tests": ["pytest"],},
 )
