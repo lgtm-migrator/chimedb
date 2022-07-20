25ddfc2491d9c28aa08051814a0915d66e8208e0
ljgray <liam.j.gray@usask.ca>
2022-07-20 16:59:34 -0700

    feat(init): add lazy version calculation

--- chimedb/core/__init__.py
+++ chimedb/core/__init__.py
@@ -118,7 +118,17 @@ from .connectdb import close, test_enable
 from .context import atomic
 from . import mediawiki
 
-from ._version import get_versions
 
-__version__ = get_versions()["version"]
-del get_versions
+def __getattr__(name: str):
+    """Custom handling for __version__ global variable."""
+    if name == "__version__":
+        # No need to re-get __version__ if it already exists
+        v = globals().get(name, None)
+        if not v:
+            from ._version import get_versions
+
+            v = get_versions()["version"]
+            globals()[name] = v
+        return v
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
